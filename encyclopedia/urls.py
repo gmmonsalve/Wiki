fcf301ac9e510c5fc31c239f8f32ec6450433fc3
gmmosnalve <gmmonsalve@uninorte.edu.co>
2020-07-14 22:32:10 -0500

    edit path

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("edit_md",views.edit_md,name="edit_md"),
     path("<str:name>",views.page_redirect, name="page_redirect")
+    
 ]
