345c6dc5f110b7ccd0501f8e84682d5ff3981745
gmmosnalve <gmmonsalve@uninorte.edu.co>
2020-07-16 16:33:33 -0500

    search view

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.http import HttpResponseRedirect
+from django import forms
 from . import util
+from django.urls import reverse
 import markdown2
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
     })
 
 def edit_md(request):
@@ -25,3 +28,19 @@ def page_redirect(request,name):
             "page": "<h1>Page Not found</h1>"
         })
 
+def search(request):
+    q = request.GET.get("q", None)
+    if q:
+        search = q
+        content = util.list_entries()
+        result = [i for i in content if search.lower() in i.lower() or search.upper() in i.upper()] 
+        print(result)
+        if search in result:
+             return redirect('page_redirect', name=search)
+        else:
+            return render(request, "encyclopedia/search.html",{
+                "search": search,
+                "entries": result
+            })
+    else:
+        return redirect('index')
