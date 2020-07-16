54e3e6022e760d688cd34343ea3d0bf47a70f0cb
gmmosnalve <gmmonsalve@uninorte.edu.co>
2020-07-16 16:29:58 -0500

    search path

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("edit_md",views.edit_md,name="edit_md"),
-    path("<str:name>",views.page_redirect, name="page_redirect")
+    path("<str:name>",views.page_redirect, name="page_redirect"),
+    path("search/",views.search, name="search") 
     
 ]
