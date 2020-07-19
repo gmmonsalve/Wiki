15e7e481584d45e71fc79653825b170e6a9dd7ab
gmmosnalve <gmmonsalve@uninorte.edu.co>
2020-07-18 21:32:47 -0500

    create path

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("edit/<str:page>",views.edit, name="edit"),
     path("<str:name>",views.page_redirect, name="page_redirect"),
     path("save_changes/<str:page_name>",views.save_changes,name="save_changes"),
-    path("search/",views.search, name="search") 
+    path("search/",views.search, name="search"),
+    path("create/",views.create,name="create")
     
 ]
