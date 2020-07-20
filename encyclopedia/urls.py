a6a876b460229c0a15cff3d29434f68bca004e0f
gmmosnalve <gmmonsalve@uninorte.edu.co>
2020-07-19 21:03:27 -0500

    new rand path, wiki path fixed

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("edit/<str:page>",views.edit, name="edit"),
-    path("<str:name>",views.page_redirect, name="page_redirect"),
+    path("wiki/<str:name>",views.page_redirect, name="page_redirect"),
     path("save_changes/<str:page_name>",views.save_changes,name="save_changes"),
     path("search/",views.search, name="search"),
-    path("create/",views.create,name="create")
-    
+    path("create",views.create,name="create"),
+    path("rand",views.rand,name="rand")
 ]
